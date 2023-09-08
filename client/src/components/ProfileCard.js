841fdfcefe72871bc548d754c124321ec757a124
kartik kumar <gtgkartik@gmail.com>
2023-09-08 11:49:17 +0530

    profile card updated with firebase details

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -1,20 +1,48 @@
-import image from "../static/images/profileimg.png";
+import React, { useEffect, useState } from "react";
+import { auth } from "../Firebase";
 
 const ProfileCard = (props) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // Listen for changes in authentication state
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in, you can access user properties here
+        setUser(user);
+      } else {
+        // User is signed out
+        setUser(null);
+      }
+    });
+
+    // Unsubscribe when the component unmounts
+    return () => unsubscribe();
+  }, []);
+
+  // Check if the user is available before accessing user properties
+  if (!user) {
+    // Return null or a loading indicator if user is not available yet
+    return null;
+  }
+
   return (
     <>
       <div
         className={`ls:w-[209px] ls:h-[54px] sm:w-[180px] sm:h-[60px] bg-${props.color} rounded-[6px] flex items-center justify-center space-x-3`}
       >
-        <img
-          src={image}
-          className={`rounded-[2px] border-[1px] border-${props.textcolor} w-[36px] `}
-        />
+        {user.photoURL && (
+          <img
+            src={user.photoURL}
+            alt={user.displayName}
+            className={`rounded-[2px] border-[1px] border-${props.textcolor} w-[36px] `}
+          />
+        )}
 
         <h1
           className={`text-[18px] text-${props.textcolor} font-juneauRegular1`}
         >
-          Kartik Kumar
+          {user.displayName}
         </h1>
       </div>
     </>
